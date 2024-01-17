9ab112ee88ad00ed1bebf214b53cd7ec4f2868c6
kaviraj2003 <2112064@nec.edu.in>
2024-01-17 20:04:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EyeDetection from './eyedetection';
+
+function App() {
+  return (
+    <div>
+        <EyeDetection/>
+    </div>
+  );
+}
+
+export default App;
